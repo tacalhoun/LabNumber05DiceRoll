73f5d51d84c110306a2b5cc2ec503d5312f4d166
Tresa Casey-Calhoun <homeaallc@gmail.com>
2017-07-14 15:58:54 -0400

    fixed do while commit

--- src/LabNumber5.java
+++ src/LabNumber5.java
@@ -8,31 +8,33 @@ import java.util.Scanner;
 public class LabNumber5 {
     public static void main(String[] args) {
         int userInput;
-    //    String answer;
+        String answer;
 
         Scanner scan = new Scanner(System.in);
+    //    scan.nextLine();
 
 
         System.out.println("Welcome to the Grand Circus Casino!");
-
+    do {
         System.out.print("How many sides should each die have?  ");
         userInput = scan.nextInt();
+        scan.nextLine();
 
         System.out.println("Roll 1...");
         System.out.println(roll(userInput));
         System.out.println(roll(userInput));
 
+     //   System.out.println("Roll 2...");
 
-        /*    do {
+
                 System.out.println("Roll again? (y/n):   ");
                 answer = scan.nextLine();
-                System.out.println();
-            } while (answer.equalsIgnoreCase("yes"));
-        */
+            } while (answer.equalsIgnoreCase("y"));
+
         }
 
     public static int roll(int n) {
         Random roll1 = new Random();
-        return (roll1.nextInt(n));
+        return 1 + (roll1.nextInt(n));
         }
     }
